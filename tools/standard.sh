3cbb8e51fbc164added6359dd1def261c75b93db
Muhammed Ensar Canbay <mecanbay@pm.me>
2022-05-15 12:56:27 +0300

    standard toollar eklendi.

---
+++ tools/standard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+package=( tilix gedit firefox gedit vlc nomacs guake net-tools curl openvpn kazam flameshot )
+
+for a in "${package[@]}"
+do
+  if [ "$(dpkg -s $a 2>/dev/null)" ]; then
+      echo "[+] ---> $a zaten sistemde yüklü."
+  else
+     echo "[!] ---> $a aracı yükleniyor..."
+     sudo apt install -y $a
+  fi
+done
