2e6ea541589186d7af1009898a3ae5f30b893eb4
Muhammed Ensar Canbay <mecanbay@pm.me>
2022-05-26 15:49:54 +0300

    python3-pip paketi eklendi

--- tools/standard.sh
+++ tools/standard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-package=(tilix gedit firefox gedit vlc nomacs guake net-tools curl openvpn kazam flameshot)
+package=(tilix gedit firefox gedit vlc nomacs guake net-tools curl openvpn kazam flameshot python3-pip)
 
 for a in "${package[@]}"; do
   if [ "$(dpkg -s $a 2>/dev/null)" ]; then
